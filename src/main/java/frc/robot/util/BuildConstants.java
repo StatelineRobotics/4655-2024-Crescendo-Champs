8250e1262c0c7dfda4e368d4fe95d76f24bb1a42
Stateline Robotics FRC Team 4655 <59455257+StatelineRobotics@users.noreply.github.com>
2024-07-22 18:30:23 -0500

    mukwongo

---
+++ src/main/java/frc/robot/util/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "4655-2024-Crescendo-Champs";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 6;
+  public static final String GIT_SHA = "3b50369d752c65a7375f740c80a3efaf563991f4";
+  public static final String GIT_DATE = "2024-04-26 21:04:43 GMT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2024-07-22 22:17:51 GMT";
+  public static final long BUILD_UNIX_TIME = 1721686671030L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
